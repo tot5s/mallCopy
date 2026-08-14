242ad3bc0f0fc32738e1e5618095b3d70b987b79
tot5s <toto_min4016@naver.com>
2026-08-14 13:26:46 +0900

    pacage-lock.json 파일 제외

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,7 +4,11 @@
 export default function Header() {
     return (
         <div className="bg-amber-50 h-20">
-
+            <div className="flex items-center justify-between">
+                <div> logo </div>
+                
+                <div> user info</div>
+            </div>
         </div>
     )
 }
